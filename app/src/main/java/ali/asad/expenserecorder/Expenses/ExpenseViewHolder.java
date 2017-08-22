d5d46c04fd25423e24781fa71f4f785eba846498
AsadAli <14asadali@gmail.com>
2017-08-22 18:13:49 +0500

    In this commit Recycler view is now added to the category show  fragment with few other changes

---
+++ app/src/main/java/ali/asad/expenserecorder/Expenses/ExpenseViewHolder.java
@@ -0,0 +1,21 @@
+package ali.asad.expenserecorder.Expenses;
+
+import android.support.v7.widget.RecyclerView;
+import android.content.DialogInterface;
+import android.view.View;
+
+/**
+ * Created by AsadAli on 22-Aug-17.
+ */
+
+public class ExpenseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public ExpenseViewHolder(View itemView) {
+        super(itemView);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/ali/asad/expenserecorder/Incomes/IncomeViewHolder.java
@@ -0,0 +1,19 @@
+package ali.asad.expenserecorder.Incomes;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by AsadAli on 22-Aug-17.
+ */
+
+public class IncomeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public IncomeViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
