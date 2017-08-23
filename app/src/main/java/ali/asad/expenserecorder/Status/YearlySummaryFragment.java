c8238bf03bd34fa72e23f1ff6650f02fae2a5460
AsadAli <14asadali@gmail.com>
2017-08-23 17:55:09 +0500

    A basic pie chart is added, Handleled glitch while entering incomes and expenses, Recycler  view is now working for all showing fragments, swiping left and right will give you next months on  showing fragments and swiping up and down on wallet status fragment will give next months.

--- app/src/main/java/ali/asad/expenserecorder/Status/YearlySummaryFragment.java
+++ app/src/main/java/ali/asad/expenserecorder/Status/YearlySummaryFragment.java
@@ -5,7 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ali.asad.expenserecorder.R;
 
@@ -15,7 +25,11 @@ import static ali.asad.expenserecorder.Status.StatusTabFragment.adpYear;
  * Created by AsadAli on 03-Aug-17.
  */
 
-public class YearlySummaryFragment extends Fragment {
+public class YearlySummaryFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    String months[] = {"January", "February", "March", "April", "May",
+            "June", "July", "August", "September", "October", "November", "December"};
+    PieChart pieChart;
     public static Spinner year;
 
     @Override
@@ -25,9 +39,45 @@ public class YearlySummaryFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.status_yearly_fragment, container, false);
         System.out.println("Yearly Summary Open");
 
+        pieChart = (PieChart) rootView.findViewById(R.id.pieChart);
         year = (Spinner) rootView.findViewById(R.id.yearSpinnerSummary);
         year.setAdapter(adpYear);
+        year.setOnItemSelectedListener(this);
+
         return rootView;
     }
 
+    private void setUpPieChart() {
+        StatusDBhelper db = new StatusDBhelper(getActivity());
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < months.length; i++) {
+            String Val;
+            if (i < 10)
+                Val = "0" + i;
+            else
+                Val = "" + i;
+
+            pieEntries.add(new PieEntry(db.getExpenses(Val,""+year.getSelectedItem()),months[i]));
+        }
+
+        PieDataSet dataset = new PieDataSet(pieEntries, "Expenses of Year "+year.getSelectedItem());
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataset);
+        pieChart.setData(data);
+        pieChart.animateY(1000);
+        pieChart.invalidate();
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        setUpPieChart();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
